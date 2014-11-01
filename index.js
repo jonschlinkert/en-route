1712d5c7a7483156fb4dbc430d993cae48098359
jonschlinkert <dev@sellside.com>
2014-10-31 21:03:20 -0400

    minor formatting

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * Most of this code is from [expressjs](https://github.com/strongloop/express)
+ *
+ * Note that material changes were made to enable handling
+ * file objects instead of requests and responses.
+ */
+
 var Route = require('./lib/route');
 var Router = require('./lib');
 

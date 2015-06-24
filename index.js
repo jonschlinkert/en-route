42f6ed8af3ac3b6e34f42102d4ad7fc123476117
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-24 13:44:13 -0400

    comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 /**
- * Although material changes were made to enable handling file
- * objects instead of requests and responses, much of this code
- * was originally from [expressjs](https://github.com/strongloop/express)
+ * Some of this code was originally from [expressjs](https://github.com/strongloop/express)
  * Copyright (c) 2009-2014 TJ Holowaychuk <tj@vision-media.ca>
  */
 

443b374d55955512181c7f584dc447349dce6e84
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-26 07:47:58 -0400

    fix accidentally-stripped comment banner

--- index.js
+++ index.js
@@ -2,15 +2,19 @@
 
 /*!
  * en-route <https://github.com/jonschlinkert/en-route>
- *
- * Copyright (c) 2009-2014, 2017, Jon Schlinkert.
+ * Some of this code was originally from
+ * [expressjs](https://github.com/strongloop/express)
+ * Copyright (c) 2009-2014 TJ Holowaychuk <tj@vision-media.ca>
+ * Copyright (c) 2015-2017, Jon Schlinkert.
  * Released under the MIT License.
  */
 
+var Layer = require('./lib/layer');
 var Route = require('./lib/route');
 var Router = require('./lib');
 
 module.exports = Router;
 module.exports.Route = Route;
 module.exports.Router = Router;
+module.exports.Layer = Layer;
 
